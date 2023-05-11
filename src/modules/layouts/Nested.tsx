a51001082e14017fb61db91424e95f6802603c21
newb1er <taiwanmichael429@gmail.com>
2023-05-11 13:19:24 +0800

    feat: scroll area

--- src/modules/layouts/Nested.tsx
+++ src/modules/layouts/Nested.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon } from "@chakra-ui/icons";
-import { Flex, Center, IconButton, Box, Wrap } from "@chakra-ui/react";
+import { Flex, Center, IconButton, Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import type { MouseEventHandler, PropsWithChildren } from "react";
 
@@ -14,21 +14,31 @@ export default function NestedLayout({
   const router = useRouter();
   const buttonCallback = backButtonCallback ?? (() => router.back());
 
-  return (
-    <Flex direction="column">
-      <Center w="full" h="80px" fontSize="22px" fontWeight="bold">
-        <IconButton
-          aria-label="Back"
-          icon={<ChevronLeftIcon />}
-          fontSize="24px"
-          variant="ghost"
-          position="absolute"
-          left="4px"
-          onClick={buttonCallback}
-        />
-        {title}
-      </Center>
+  const TopBar = () => (
+    <Center w="full" h="80px" fontSize="22px" fontWeight="bold">
+      <IconButton
+        aria-label="Back"
+        icon={<ChevronLeftIcon />}
+        fontSize="24px"
+        variant="ghost"
+        position="absolute"
+        left="4px"
+        onClick={buttonCallback}
+      />
+      {title}
+    </Center>
+  );
+
+  const ScrollArea = () => (
+    <Box flex="1" overflow="scroll">
       {children}
+    </Box>
+  );
+
+  return (
+    <Flex direction="column" maxH="100vh" maxW="100vw">
+      <TopBar />
+      <ScrollArea />
     </Flex>
   );
 }
--- src/modules/layouts/Top.tsx
+++ src/modules/layouts/Top.tsx
@@ -1,21 +1,31 @@
-import { Flex, Center } from "@chakra-ui/react";
+import { Flex, Center, Box } from "@chakra-ui/react";
 import type { PropsWithChildren } from "react";
 
 export default function TopLayout({ children }: PropsWithChildren) {
-  return (
-    <Flex direction="column">
-      <Center
-        w="full"
-        h="80px"
-        bg="black"
-        color="white"
-        fontSize="36px"
-        lineHeight="7"
-        fontWeight="extrabold"
-      >
-        CoRide
-      </Center>
+  const TopBar = () => (
+    <Center
+      w="full"
+      h="80px"
+      bg="black"
+      color="white"
+      fontSize="36px"
+      lineHeight="7"
+      fontWeight="extrabold"
+    >
+      CoRide
+    </Center>
+  );
+
+  const ScrollArea = () => (
+    <Box flex="1" overflow="scroll">
       {children}
+    </Box>
+  );
+
+  return (
+    <Flex direction="column" maxH="100vh" maxW="100vw">
+      <TopBar />
+      <ScrollArea />
     </Flex>
   );
 }
