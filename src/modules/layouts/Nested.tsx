1bea62c851b87276aca3e0c7633aa29ce646b9a8
newb1er <taiwanmichael429@gmail.com>
2023-05-15 22:25:54 +0800

    fix: avoid child component re-render

--- src/modules/layouts/Nested.tsx
+++ src/modules/layouts/Nested.tsx
@@ -3,6 +3,33 @@ import { Flex, Center, IconButton, Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import type { MouseEventHandler, PropsWithChildren } from "react";
 
+const TopBar = ({
+  title,
+  buttonCallback,
+}: {
+  title: string;
+  buttonCallback: MouseEventHandler<HTMLButtonElement>;
+}) => (
+  <Center w="full" h="80px" fontSize="22px" fontWeight="bold">
+    <IconButton
+      aria-label="Back"
+      icon={<ChevronLeftIcon />}
+      fontSize="24px"
+      variant="ghost"
+      position="absolute"
+      left="4px"
+      onClick={buttonCallback}
+    />
+    {title}
+  </Center>
+);
+
+const ScrollArea = ({ children }: PropsWithChildren) => (
+  <Box flex="1" overflow="scroll">
+    {children}
+  </Box>
+);
+
 export default function NestedLayout({
   children,
   title,
@@ -14,31 +41,10 @@ export default function NestedLayout({
   const router = useRouter();
   const buttonCallback = backButtonCallback ?? (() => router.back());
 
-  const TopBar = () => (
-    <Center w="full" h="80px" fontSize="22px" fontWeight="bold">
-      <IconButton
-        aria-label="Back"
-        icon={<ChevronLeftIcon />}
-        fontSize="24px"
-        variant="ghost"
-        position="absolute"
-        left="4px"
-        onClick={buttonCallback}
-      />
-      {title}
-    </Center>
-  );
-
-  const ScrollArea = () => (
-    <Box flex="1" overflow="scroll">
-      {children}
-    </Box>
-  );
-
   return (
     <Flex direction="column" maxH="100vh" maxW="100vw">
-      <TopBar />
-      <ScrollArea />
+      <TopBar title={title} buttonCallback={buttonCallback} />
+      <ScrollArea>{children}</ScrollArea>
     </Flex>
   );
 }
