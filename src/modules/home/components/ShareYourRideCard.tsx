c12db6a192873a7ed7d9e2f837c85855b921c8f4
ANITA-0604 <anita900604@gmail.com>
2023-06-04 14:52:47 +0800

    fix : revise style of homepage button

--- src/modules/home/components/ShareYourRideCard.tsx
+++ src/modules/home/components/ShareYourRideCard.tsx
@@ -22,7 +22,8 @@ export default function ShareYourRideCard() {
           >
             <Text fontWeight={700}>Become a driver and pick up passengers</Text>
             <Button
-              bg="white"
+              colorScheme="white"
+              color={"black"}
               marginTop="10px"
               height="min-content"
               borderRadius="20px"
