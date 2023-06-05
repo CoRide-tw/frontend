7ed4487cbd8ac4ac64c6df16c099e39a24803f3b
ericyangchen <yericchenyang@gmail.com>
2023-06-06 01:24:54 +0800

    fix: misc small issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,13 +2,19 @@ import TopLayout from "@/modules/layouts/Top";
 import RiderSearchInput from "@/modules/components/RiderSearchInput";
 import ShareYourRideCard from "@/modules/home/components/ShareYourRideCard";
 import YourTripsSection from "@/modules/home/components/YourTripsSection";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
-    <TopLayout>
-      <RiderSearchInput />
-      <ShareYourRideCard />
-      <YourTripsSection />
-    </TopLayout>
+    <>
+      <Head>
+        <title>Home - CoRide</title>
+      </Head>
+      <TopLayout>
+        <RiderSearchInput />
+        <ShareYourRideCard />
+        <YourTripsSection />
+      </TopLayout>
+    </>
   );
 }
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,13 +2,18 @@ import DriveCards from "@/modules/profile/components/HistoryTripCard";
 import NestedLayout from "@/modules/layouts/Nested";
 import HistoryPosts from "@/modules/profile/components/HistoryPosts";
 import PersonalDetails from "@/modules/profile/components/PersonalDetails";
-import { Hide } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Profile() {
   return (
-    <NestedLayout title="Profile">
-      <PersonalDetails />
-      <HistoryPosts />
-    </NestedLayout>
+    <>
+      <Head>
+        <title>Profile - CoRide</title>
+      </Head>
+      <NestedLayout title="Profile">
+        <PersonalDetails />
+        <HistoryPosts />
+      </NestedLayout>
+    </>
   );
 }
