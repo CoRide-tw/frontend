9ef524e9747dea331ca76f396a707aa9a3f8aeaa
ANITA-0604 <anita900604@gmail.com>
2023-06-07 16:16:02 +0800

    feat : current section

---
+++ src/modules/home/components/CurrentRouteSection.tsx
@@ -0,0 +1,46 @@
+import { authFetcher } from "@/modules/api/fetcher";
+import { useUser } from "@/modules/api/swr/useUser";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { Flex, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+export default function CurrentRequestSection() {
+  const { user } = useUser();
+  const router = useRouter();
+  const shouldFetch = !!Number(user?.id);
+  const { data, error, isLoading } = useSWR(
+    () => (shouldFetch ? `/request?riderId=${Number(user.id)}` : null),
+    authFetcher
+  );
+
+  let cnt = data?.filter(
+    (item: any) =>
+      item.status === "pending" && new Date(item.pickupEndTime) > new Date()
+  ).length;
+  console.log(data);
+
+  if (cnt === 0) return null;
+  else
+    return (
+      <Flex margin={"20px"} direction={"column"}>
+        <Text fontSize="xl" fontWeight={"600"} margin={"10px 0"}>
+          Requests
+        </Text>
+        <Flex
+          borderRadius={"10px"}
+          border={"1px solid #E2E8F0"}
+          padding={"10px "}
+          fontWeight={"500"}
+          justify={"space-between"}
+          align={"center"}
+          onClick={() => {
+            router.push("/driver/requests?new=1&routeId=");
+          }}
+        >
+          You have {cnt} requests waiting.
+          <ChevronRightIcon />
+        </Flex>
+      </Flex>
+    );
+}
