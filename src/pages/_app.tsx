6b671f1617357b9f796cc92ec2a46f4051bee9e7
ericyangchen <yericchenyang@gmail.com>
2023-06-07 19:49:09 +0800

    feat: add manifeest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <title>CoRide</title>
       </Head>
       <ChakraProvider theme={theme}>
